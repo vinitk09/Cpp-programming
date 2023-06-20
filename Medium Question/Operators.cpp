c17ead4ecb8cb1424e623aacc121e46cf868b96d
vinit <vinitkumar8092@gmail.com>
2023-06-20 22:47:05 +0530

    changed

---
+++ Medium Question/Operators.cpp
@@ -0,0 +1,36 @@
+// Program to display arithmetic operators using switch case.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	char op;
+	float num1, num2;
+	cout << "Enter the 1st operand: ";
+	cin >> num1;
+	cout << "Enter operator either + or - or * or / : ";
+	cin >> op;
+	cout << "Enter 2nd operand: ";
+	cin >> num2;
+	cout << endl;
+	switch (op)
+	{
+	case '+':
+		cout << "Result is " << num1 + num2 << endl;
+		break;
+	case '-':
+		cout << "Result is " << num1 - num2 << endl;
+		break;
+	case '*':
+		cout << "Result is " << num1 * num2 << endl;
+		break;
+	case '/':
+		cout << "Result is " << num1 / num2 << endl;
+		break;
+	default:
+		cout << "Error! Operator is not correct";
+		break;
+	}
+	system("pause");
+	return 0;
+}
